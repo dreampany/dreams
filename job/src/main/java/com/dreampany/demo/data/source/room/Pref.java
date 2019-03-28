2ff3e3219fb6d6e5025b902708bd59e66d7cac58
Hawladar Roman <hawladar.roman@bjitgroup.com>
2019-03-28 12:20:44 +0600

    # job first commit

---
+++ job/src/main/java/com/dreampany/demo/data/source/room/Pref.java
@@ -0,0 +1,22 @@
+package com.dreampany.demo.data.source.room;
+
+import android.content.Context;
+
+import com.dreampany.frame.data.source.pref.FramePref;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * Created by Hawladar Roman on 3/7/18.
+ * Dreampany Ltd
+ * dreampanymail@gmail.com
+ */
+@Singleton
+public class Pref extends FramePref {
+    
+    @Inject
+    Pref(Context context) {
+        super(context);
+    }
+}
---
+++ job/src/main/java/com/dreampany/demo/vm/DemoViewModel.java
@@ -0,0 +1,34 @@
+package com.dreampany.demo.vm;
+
+import android.app.Application;
+
+import com.dreampany.frame.misc.AppExecutors;
+import com.dreampany.frame.misc.ResponseMapper;
+import com.dreampany.frame.misc.RxMapper;
+import com.dreampany.frame.vm.BaseViewModel;
+import com.dreampany.network.manager.NetworkManager;
+import com.dreampany.demo.data.model.Demo;
+import com.dreampany.demo.ui.model.DemoItem;
+import com.dreampany.demo.ui.model.UiTask;
+
+import javax.inject.Inject;
+
+/**
+ * Created by Hawladar Roman on 6/29/2018.
+ * BJIT Group
+ * hawladar.roman@bjitgroup.com
+ */
+public class DemoViewModel extends BaseViewModel<Demo, DemoItem, UiTask<Demo>> {
+
+    private final NetworkManager network;
+
+    @Inject
+    DemoViewModel(Application application,
+                  RxMapper rx,
+                  AppExecutors ex,
+                  ResponseMapper rm,
+                  NetworkManager network) {
+        super(application, rx, ex, rm);
+        this.network = network;
+    }
+}

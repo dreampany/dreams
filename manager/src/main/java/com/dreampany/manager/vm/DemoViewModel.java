2b2ea4b8287eb2f8c3a2e84d056c87c194905fa0
Hawladar Roman <hawladar.roman@bjitgroup.com>
2019-05-13 15:43:28 +0600

    # manager committed

---
+++ manager/src/main/java/com/dreampany/manager/vm/DemoViewModel.java
@@ -0,0 +1,34 @@
+package com.dreampany.manager.vm;
+
+import android.app.Application;
+
+import com.dreampany.frame.misc.AppExecutors;
+import com.dreampany.frame.misc.ResponseMapper;
+import com.dreampany.frame.misc.RxMapper;
+import com.dreampany.frame.vm.BaseViewModel;
+import com.dreampany.network.manager.NetworkManager;
+import com.dreampany.manager.data.model.Demo;
+import com.dreampany.manager.ui.model.DemoItem;
+import com.dreampany.manager.ui.model.UiTask;
+
+import javax.inject.Inject;
+
+/**
+ * Created by Hawladar Roman on 6/29/2018.
+ * BJIT Group
+ * hawladar.roman@bjitgroup.com
+ */
+public class DemoViewModel extends BaseViewModel<Demo, DemoItem, UiTask<Demo>> {
+
+    private final NetworkManager network;
+
+    @Inject
+    DemoViewModel(Application application,
+                  RxMapper rx,
+                  AppExecutors ex,
+                  ResponseMapper rm,
+                  NetworkManager network) {
+        super(application, rx, ex, rm);
+        this.network = network;
+    }
+}

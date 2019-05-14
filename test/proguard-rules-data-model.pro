216482b90292fc2a39c90f375c48dca6fd9deeb6
roman <dreampanymail@gmail.com>
2019-05-14 20:57:44 +0600

    # test module is committed

---
+++ test/proguard-rules-data-model.pro
@@ -0,0 +1,5 @@
+-keep class com.dreampany.demo.data.model.** { *; }
+-keepclassmembers class com.dreampany.demo.data.model.** { *; }
+
+-keep class com.dreampany.demo.ui.model.** { *; }
+-keepclassmembers class com.dreampany.demo.ui.model.** { *; }
---
+++ test/src/main/kotlin/com/dreampany/test/injector/vm/ViewModelModule.kt
@@ -0,0 +1,36 @@
+package com.dreampany.test.injector.vm
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.dreampany.test.vm.DemoViewModel
+import com.dreampany.test.vm.MoreViewModel
+import com.dreampany.frame.vm.ViewModelKey
+import com.dreampany.frame.vm.factory.ViewModelFactory
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+import javax.inject.Singleton
+
+
+/**
+ * Created by Hawladar Roman on 5/31/2018.
+ * BJIT Group
+ * hawladar.roman@bjitgroup.com
+ */
+@Module
+abstract class ViewModelModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MoreViewModel::class)
+    abstract fun bindMoreViewModel(vm: MoreViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(DemoViewModel::class)
+    abstract fun bindDemoViewModel(demoViewModel: DemoViewModel): ViewModel
+
+    @Singleton
+    @Binds
+    abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
+}

f904712421cd8096c2f0842c7314ca8f93b14036
roman <dreampanymail@gmail.com>
2019-05-25 17:52:39 +0600

    # keep firebase into frame

---
+++ frame/src/main/kotlin/com/dreampany/network/data/enums/NetworkType.kt
@@ -0,0 +1,11 @@
+package com.dreampany.network.data.enums
+
+
+/**
+ * Created by Hawladar Roman on 6/9/2018.
+ * Dreampany Ltd
+ * dreampanymail@gmail.com
+ */
+enum class NetworkType {
+    WIFI, HOTSPOT, BLUETOOTH;
+}
---
+++ frame/src/main/kotlin/com/dreampany/network/misc/Constants.kt
@@ -0,0 +1,15 @@
+package com.dreampany.network.misc
+
+
+/**
+ * Created by Hawladar Roman on 8/18/2018.
+ * Dreampany Ltd
+ * dreampanymail@gmail.com
+ */
+object Constants {
+    object SECURITY {
+        const val WEP = "WEP"
+        const val PSK = "PSK"
+        const val EAP = "EAP"
+    }
+}

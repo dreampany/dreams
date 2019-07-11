ec388a711baffb7263d457106633f38e6bec963e
Hawladar Roman <hawladar.roman@bjitgroup.com>
2019-07-11 15:12:44 +0600

    # home page designed

--- translate/src/main/java/com/dreampany/translate/ui/fragment/HomeFragment.java
+++ translate/src/main/java/com/dreampany/translate/ui/fragment/HomeFragment.java
@@ -1,15 +1,20 @@
 package com.dreampany.translate.ui.fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.ObservableArrayList;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.dreampany.frame.misc.ActivityScope;
-import com.dreampany.frame.ui.fragment.BaseFragment;
 import com.dreampany.frame.ui.fragment.BaseMenuFragment;
 import com.dreampany.translate.R;
+import com.dreampany.translate.databinding.ContentRecyclerBinding;
+import com.dreampany.translate.databinding.ContentTopStatusBinding;
+import com.dreampany.translate.databinding.FragmentHomeBinding;
+import com.dreampany.translate.ui.adapter.TranslationAdapter;
+import com.jaiselrahman.hintspinner.HintSpinner;
 
 import javax.inject.Inject;
 
@@ -28,6 +33,11 @@ public class HomeFragment extends BaseMenuFragment {
 
     @Inject
     ViewModelProvider.Factory factory;
+    private FragmentHomeBinding binding;
+    private ContentTopStatusBinding bindStatus;
+   // private ContentRecyclerBinding bindRecycler;
+
+    private TranslationAdapter adapter;
 
     @Inject
     public HomeFragment() {
@@ -35,10 +45,10 @@ public class HomeFragment extends BaseMenuFragment {
 
     @Override
     public int getLayoutId() {
-        return R.layout.fragment_tabpager_fixed;
+        return R.layout.fragment_home;
     }
 
-    @Override
+/*    @Override
     public int getMenuId() {
         return R.menu.menu_home;
     }
@@ -46,11 +56,12 @@ public class HomeFragment extends BaseMenuFragment {
     @Override
     public int getSearchMenuItemId() {
         return R.id.item_search;
-    }
+    }*/
 
     @Override
     protected void onStartUi(@Nullable Bundle state) {
         initView();
+        initRecycler();
     }
 
     @Override
@@ -58,20 +69,48 @@ public class HomeFragment extends BaseMenuFragment {
 
     }
 
-
-    @NonNull
-    @Override
-    public ViewModelProvider.Factory get() {
-        return factory;
-    }
-
-    @Override
-    public boolean onQueryTextChange(@NonNull String newText) {
-        BaseFragment fragment = getCurrentFragment();
-        return fragment != null && fragment.onQueryTextChange(newText);
-    }
-
     private void initView() {
         setTitle(R.string.home);
+        binding = (FragmentHomeBinding) super.binding;
+        bindStatus = binding.layoutTopStatus;
+        //bindRecycler = binding.layoutRecycler;
+
+
+        binding.stateful.setStateView(NONE, LayoutInflater.from(getContext()).inflate(R.layout.item_none, null));
+
+        binding.fab.setOnClickListener(this);
+
+
+        /*vm = ViewModelProviders.of(this, factory).get(WordViewModelKt.class);
+        vm.setUiCallback(this);
+        vm.observeUiState(this, this::processUiState);
+        vm.observeOutputsOfString(this, this::processResponseOfString);
+        vm.observeOutputs(this, this::processResponse);
+        vm.observeOutput(this, this::processSingleResponse);*/
+
+
+    }
+
+    private void initRecycler() {
+/*        binding.setItems(new ObservableArrayList<>());
+        adapter = new TranslationAdapter(this);
+        adapter.setStickyHeaders(false);*/
+/*        scroller = new OnVerticalScrollListener() {
+            @Override
+            public void onScrollingAtEnd() {
+
+            }
+        };*/
+/*        ViewUtil.setRecycler(
+                adapter,
+                bindRecycler.recycler,
+                new SmoothScrollLinearLayoutManager(getContext()),
+                new FlexibleItemDecoration(getContext())
+                        .addItemViewType(R.layout.item_word, vm.getItemOffset())
+                        .withEdge(true),
+                null,
+                scroller,
+                null
+        );*/
     }
 }

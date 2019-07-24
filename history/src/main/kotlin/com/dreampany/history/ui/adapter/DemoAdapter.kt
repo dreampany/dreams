aa342a801600b72426bf32480c25f57026e55e31
Hawladar Roman <hawladar.roman@bjitgroup.com>
2019-07-24 14:07:17 +0600

    # history commited

---
+++ history/src/main/kotlin/com/dreampany/history/ui/adapter/DemoAdapter.kt
@@ -0,0 +1,13 @@
+package com.dreampany.history.ui.adapter
+
+import com.dreampany.history.ui.model.DemoItem
+import com.dreampany.frame.ui.adapter.SmartAdapter
+
+/**
+ * Created by Roman-372 on 7/24/2019
+ * Copyright (c) 2019 bjit. All rights reserved.
+ * hawladar.roman@bjitgroup.com
+ * Last modified $file.lastModified
+ */
+class DemoAdapter(listener: Any?) : SmartAdapter<DemoItem>(listener) {
+}

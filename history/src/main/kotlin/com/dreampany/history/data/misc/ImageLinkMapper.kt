af8c3b25b3105e836a8cf379941213d1bfbaf554
Hawladar Roman <hawladar.roman@bjitgroup.com>
2019-07-30 16:29:17 +0600

    # image link form

--- history/src/main/kotlin/com/dreampany/history/data/misc/ImageLinkMapper.kt
+++ history/src/main/kotlin/com/dreampany/history/data/misc/ImageLinkMapper.kt
@@ -1,10 +1,29 @@
 package com.dreampany.history.data.misc
 
+import com.dreampany.frame.data.model.ImageLink
+import com.dreampany.frame.misc.SmartCache
+import com.dreampany.frame.misc.SmartMap
+import com.dreampany.history.data.enums.HistoryType
+import com.dreampany.history.data.model.History
+import com.dreampany.history.data.source.remote.WikiHistory
+import com.dreampany.history.misc.ImageLinkAnnote
+import javax.inject.Inject
+import javax.inject.Singleton
+
 /**
  * Created by Roman-372 on 7/30/2019
  * Copyright (c) 2019 bjit. All rights reserved.
  * hawladar.roman@bjitgroup.com
  * Last modified $file.lastModified
  */
-class ImageLinkMapper {
+@Singleton
+class ImageLinkMapper
+@Inject constructor(
+    @ImageLinkAnnote val map: SmartMap<String, ImageLink>,
+    @ImageLinkAnnote val cache: SmartCache<String, ImageLink>
+) {
+
+/*    fun toItem(ref: String, url: String, title:String): ImageLink? {
+
+    }*/
 }
--- history/src/main/kotlin/com/dreampany/history/injector/data/SupportModule.kt
+++ history/src/main/kotlin/com/dreampany/history/injector/data/SupportModule.kt
@@ -1,9 +1,11 @@
 package com.dreampany.history.injector.data
 
+import com.dreampany.frame.data.model.ImageLink
 import com.dreampany.frame.misc.SmartCache
 import com.dreampany.frame.misc.SmartMap
 import com.dreampany.history.data.model.History
 import com.dreampany.history.misc.HistoryAnnote
+import com.dreampany.history.misc.ImageLinkAnnote
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -28,6 +30,20 @@ class SupportModule {
     @Provides
     @HistoryAnnote
     fun provideHistorySmartCache(): SmartCache<String, History> {
+        return SmartCache.newCache()
+    }
+
+    @Singleton
+    @Provides
+    @ImageLinkAnnote
+    fun provideImageLinkSmartMap(): SmartMap<String, ImageLink> {
+        return SmartMap.newMap()
+    }
+
+    @Singleton
+    @Provides
+    @ImageLinkAnnote
+    fun provideImageLinkSmartCache(): SmartCache<String, ImageLink> {
         return SmartCache.newCache()
     }
 }

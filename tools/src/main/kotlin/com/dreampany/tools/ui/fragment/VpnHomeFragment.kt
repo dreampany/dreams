1da73663a79b569599c9d9b51f7ddad1f70a4c2b
Roman <dreampanymail@gmail.com>
2019-10-08 07:35:26 +0600

    vpn ui updated

--- tools/src/main/kotlin/com/dreampany/tools/ui/fragment/VpnHomeFragment.kt
+++ tools/src/main/kotlin/com/dreampany/tools/ui/fragment/VpnHomeFragment.kt
@@ -1,9 +1,27 @@
 package com.dreampany.tools.ui.fragment
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import com.dreampany.framework.data.enums.Action
+import com.dreampany.framework.data.enums.State
+import com.dreampany.framework.data.model.Response
 import com.dreampany.framework.misc.ActivityScope
+import com.dreampany.framework.misc.exception.EmptyException
+import com.dreampany.framework.misc.exception.ExtraException
+import com.dreampany.framework.misc.exception.MultiException
+import com.dreampany.framework.ui.enums.UiState
 import com.dreampany.framework.ui.fragment.BaseMenuFragment
 import com.dreampany.tools.R
+import com.dreampany.tools.databinding.ContentTopStatusBinding
+import com.dreampany.tools.databinding.FragmentVpnHomeBinding
+import com.dreampany.tools.ui.model.ServerItem
+import com.dreampany.tools.ui.vm.ServerViewModel
+import cz.kinst.jakub.view.StatefulLayout
+import timber.log.Timber
+import java.io.IOException
 import javax.inject.Inject
 
 /**
@@ -16,6 +34,14 @@ import javax.inject.Inject
 class VpnHomeFragment
 @Inject constructor() : BaseMenuFragment() {
 
+    @Inject
+    internal lateinit var factory: ViewModelProvider.Factory
+
+    private lateinit var bind: FragmentVpnHomeBinding
+    private lateinit var bindStatus: ContentTopStatusBinding
+
+    private lateinit var vm: ServerViewModel
+
     override fun getLayoutId(): Int {
         return R.layout.fragment_vpn_home
     }
@@ -25,10 +51,87 @@ class VpnHomeFragment
     }
 
     override fun onStartUi(state: Bundle?) {
-
+initUi()
     }
 
     override fun onStopUi() {
 
+    }
+
+    private fun initUi() {
+        bind = super.binding as FragmentVpnHomeBinding
+        bindStatus = bind.layoutTopStatus
+
+        bind.stateful.setStateView(
+            UiState.DEFAULT.name,
+            LayoutInflater.from(context).inflate(R.layout.item_default, null)
+        )
+        bind.stateful.setStateView(
+            UiState.SEARCH.name,
+            LayoutInflater.from(context).inflate(R.layout.item_search, null)
+        )
+        bind.stateful.setStateView(
+            UiState.EMPTY.name,
+            LayoutInflater.from(context).inflate(R.layout.item_empty, null)
+        )
+
+        processUiState(UiState.DEFAULT)
+
+        vm = ViewModelProviders.of(this, factory).get(ServerViewModel::class.java)
+        vm.observeUiState(this, Observer { this.processUiState(it) })
+        vm.observeOutput(this, Observer { this.processSingleResponse(it) })
+    }
+
+    private fun processUiState(state: UiState) {
+        when (state) {
+            UiState.DEFAULT -> bind.stateful.setState(UiState.DEFAULT.name)
+            UiState.SHOW_PROGRESS -> if (!bind.layoutRefresh.isRefreshing()) {
+                bind.layoutRefresh.setRefreshing(true)
+            }
+            UiState.HIDE_PROGRESS -> if (bind.layoutRefresh.isRefreshing()) {
+                bind.layoutRefresh.setRefreshing(false)
+            }
+            UiState.OFFLINE -> bindStatus.layoutExpandable.expand()
+            UiState.ONLINE -> bindStatus.layoutExpandable.collapse()
+            //UiState.EXTRA -> processUiState(if (adapter.isEmpty()) UiState.EMPTY else UiState.CONTENT)
+            UiState.SEARCH -> bind.stateful.setState(UiState.SEARCH.name)
+            UiState.EMPTY -> bind.stateful.setState(UiState.EMPTY.name)
+            UiState.ERROR -> {
+            }
+            UiState.CONTENT -> bind.stateful.setState(StatefulLayout.State.CONTENT)
+        }
+    }
+
+    private fun processSingleResponse(response: Response<ServerItem>) {
+        if (response is Response.Progress<*>) {
+            val result = response as Response.Progress<*>
+            vm.processProgress(result.loading)
+        } else if (response is Response.Failure<*>) {
+            val result = response as Response.Failure<*>
+            processFailure(result.error)
+        } else if (response is Response.Result<*>) {
+            val result = response as Response.Result<ServerItem>
+            processSingleSuccess(result.state, result.action, result.data)
+        }
+    }
+
+    private fun processFailure(error: Throwable) {
+        if (error is IOException || error.cause is IOException) {
+            vm.updateUiState(UiState.OFFLINE)
+        } else if (error is EmptyException) {
+            vm.updateUiState(UiState.EMPTY)
+        } else if (error is ExtraException) {
+            vm.updateUiState(UiState.EXTRA)
+        } else if (error is MultiException) {
+            for (e in error.errors) {
+                processFailure(e)
+            }
+        }
+    }
+
+    private fun processSingleSuccess(state: State, action: Action, uiItem: ServerItem) {
+        Timber.v("Result Single Server[%s]", uiItem.item.id)
+        bind.setItem(uiItem)
+        processUiState(UiState.CONTENT)
     }
 }

3ffbbcc0645a525644692bc5074e32bdda8d02ed
Roman <hawladar.roman@bjitgroup.com>
2019-10-21 18:35:23 +0600

    twiiter sdk added

--- share-dialog/src/main/java/com/dreampany/share_dialog/MainActivity.kt
+++ share-dialog/src/main/java/com/dreampany/share_dialog/MainActivity.kt
@@ -12,6 +12,22 @@ import com.facebook.share.Sharer
 import com.facebook.share.model.ShareLinkContent
 import com.facebook.share.widget.ShareDialog
 import kotlinx.android.synthetic.main.activity_main.*
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.R.attr.data
+import com.twitter.sdk.android.core.models.Tweet
+import com.twitter.sdk.android.core.services.StatusesService
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import com.twitter.sdk.android.core.*
+import kotlin.Result
+import com.twitter.sdk.android.core.TwitterException
+import com.twitter.sdk.android.core.TwitterSession
+import com.twitter.sdk.android.core.identity.TwitterAuthClient
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +36,8 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var callbackManager: CallbackManager
     lateinit   var shareDialog: ShareDialog
+
+    lateinit var twitterAuthClient: TwitterAuthClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,20 +62,70 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener {
             share()
         }
+        twitterAuthClient = TwitterAuthClient()
+
+        val twitterSession = TwitterCore.getInstance().sessionManager.activeSession
+        if (twitterSession == null) {
+            twitterAuthClient.authorize(this, object : Callback<TwitterSession>() {
+                override fun failure(exception: TwitterException) {
+                    Log.e("Twitter", "Failed to authenticate user " + exception.message)
+                }
+
+                override fun success(result: com.twitter.sdk.android.core.Result<TwitterSession>) {
+                    Log.e("Twitter", "success + ")
+
+                }
+
+            })
+        }
+
+/*        login_button.callback = object : Callback<TwitterSession>() {
+            override fun failure(exception: TwitterException?) {
+                Log.d("Error", exception?.toString() ?: "Error");
+            }
+
+            override fun success(result: com.twitter.sdk.android.core.Result<TwitterSession>?) {
+                share()
+            }
+
+        }*/
+
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        if (twitterAuthClient != null) {
+            twitterAuthClient.onActivityResult(requestCode, resultCode, data);
+        }
         callbackManager.onActivityResult(requestCode, resultCode, data)
+        //login_button.onActivityResult(requestCode, resultCode, data);
+    }
+
+    fun login() {
+
     }
 
     fun share() {
-        if (ShareDialog.canShow(ShareLinkContent::class.java)) {
+
+/*        if (ShareDialog.canShow(ShareLinkContent::class.java)) {
             val linkContent = ShareLinkContent.Builder()
                 .setContentUrl(Uri.parse("http://developers.facebook.com/android"))
                 .build()
             shareDialog.show(linkContent)
-        }
+        }*/
+        val statusesService = TwitterCore.getInstance().apiClient.statusesService
+        val tweetCall = statusesService.update("HelloDear", null, false, null, null, null, false, false, null)
+        tweetCall.enqueue(object : Callback<Tweet>() {
+            override fun success(result: com.twitter.sdk.android.core.Result<Tweet>) {
+                Log.d("result", result?.data?.idStr ?: "Hee");
+            }
+
+            override fun failure(exception: TwitterException) {
+                Log.d("Error", exception.toString());
+            }
+
+        })
     }
 
 }

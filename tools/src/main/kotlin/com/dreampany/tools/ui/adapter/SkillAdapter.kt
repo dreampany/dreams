050696b7199b3d839e30717e4ab465a1367a29ab
Roman <dreampanymail@gmail.com>
2020-01-16 21:34:44 +0600

    Resume layout and model done

---
+++ tools/src/main/kotlin/com/dreampany/tools/ui/adapter/SkillAdapter.kt
@@ -0,0 +1,14 @@
+package com.dreampany.tools.ui.adapter
+
+import com.dreampany.framework.ui.adapter.SmartAdapter
+import com.dreampany.tools.ui.model.ResumeItem
+import com.dreampany.tools.ui.model.SkillItem
+
+/**
+ * Created by roman on 2020-01-15
+ * Copyright (c) 2020 bjit. All rights reserved.
+ * hawladar.roman@bjitgroup.com
+ * Last modified $file.lastModified
+ */
+class SkillAdapter(listener: Any? = null) : SmartAdapter<SkillItem>(listener) {
+}
---
+++ tools/src/main/kotlin/com/dreampany/tools/ui/model/ExperienceItem.kt
@@ -0,0 +1,72 @@
+package com.dreampany.tools.ui.model
+
+import android.view.View
+import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.RecyclerView
+import com.dreampany.framework.data.model.Base
+import com.dreampany.framework.ui.model.BaseItem
+import com.dreampany.tools.R
+import com.dreampany.tools.data.model.Experience
+import com.dreampany.tools.data.model.Resume
+import com.dreampany.tools.data.model.Skill
+import com.dreampany.tools.misc.Constants
+import com.google.common.base.Objects
+import eu.davidea.flexibleadapter.FlexibleAdapter
+import eu.davidea.flexibleadapter.items.IFlexible
+import java.io.Serializable
+
+/**
+ * Created by roman on 2020-01-15
+ * Copyright (c) 2020 bjit. All rights reserved.
+ * hawladar.roman@bjitgroup.com
+ * Last modified $file.lastModified
+ */
+class ExperienceItem private constructor(
+    item: Experience,
+    @LayoutRes layoutId: Int = Constants.Default.INT
+) : BaseItem<ExperienceItem.ViewHolder, Experience, String>(item, layoutId) {
+
+    companion object {
+        fun getItem(item: Experience): ExperienceItem {
+            return ExperienceItem(item, 0)
+        }
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hashCode(item.id)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || javaClass != other.javaClass) return false
+        val item = other as ServerItem
+        return Objects.equal(this.item.id, item.item.id)
+    }
+
+    override fun createViewHolder(
+        view: View,
+        adapter: FlexibleAdapter<IFlexible<RecyclerView.ViewHolder>>
+    ): ExperienceItem.ViewHolder {
+        return ExperienceItem.ViewHolder(view, adapter)
+    }
+
+    override fun filter(constraint: String): Boolean {
+        return false
+    }
+
+    class ViewHolder(view: View, adapter: FlexibleAdapter<*>) :
+        BaseItem.ViewHolder(view, adapter) {
+
+        init {
+
+        }
+
+        override fun <VH : BaseItem.ViewHolder, T : Base, S : Serializable, I : BaseItem<VH, T, S>>
+                bind(position: Int, item: I) {
+            val uiItem = item as ResumeItem
+            val item = uiItem.item
+
+
+        }
+    }
+}

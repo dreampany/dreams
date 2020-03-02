de2c3155746aae6018c4542a83d0ada1359528f4
Roman <dreampanymail@gmail.com>
2020-03-03 00:41:12 +0600

    common updated

---
+++ common/src/main/java/com/dreampany/common/d.kt
@@ -0,0 +1,8 @@
+package com.dreampany.common
+
+/**
+ * Created by roman on 3/3/20
+ * Copyright (c) 2020 bjit. All rights reserved.
+ * hawladar.roman@bjitgroup.com
+ * Last modified $file.lastModified
+ */

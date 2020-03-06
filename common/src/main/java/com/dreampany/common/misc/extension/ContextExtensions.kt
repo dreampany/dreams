83adffd39905bb05a3554c7b3e48c4ad1893d569
Roman <hawladar.roman@bjitgroup.com>
2020-03-06 11:34:17 +0600

    get current task

--- common/src/main/java/com/dreampany/common/misc/extension/ContextExtensions.kt
+++ common/src/main/java/com/dreampany/common/misc/extension/ContextExtensions.kt
@@ -1,9 +1,16 @@
 package com.dreampany.common.misc.extension
 
+import android.app.ActivityManager
+import android.app.usage.UsageStats
+import android.app.usage.UsageStatsManager
 import android.content.Context
+import android.os.Build
 import androidx.annotation.ColorRes
 import androidx.annotation.DimenRes
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by roman on 3/3/20
@@ -17,4 +24,22 @@ fun Context?.dimension(@DimenRes resId: Int): Float {
 
 fun Context?.color(@ColorRes resId: Int): Int {
     return if (this == null) 0 else ContextCompat.getColor(this, resId)
+}
+
+fun Context?.currentTask() : ActivityManager.RunningTaskInfo? {
+    val manager = this?.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager?
+    return manager?.getRunningTasks(1)?.first()
+}
+
+@RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+fun Context?.currentUsageStats():  UsageStats? {
+    val manager = this?.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager?
+    val endMillis = System.currentTimeMillis()
+    val startMillis = endMillis - TimeUnit.MINUTES.toMillis(1)
+    val stats = manager?.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, startMillis, endMillis)
+    val sortedMap = TreeMap<Long, UsageStats>()
+    stats?.forEach {
+        sortedMap.put(it.lastTimeUsed, it)
+    }
+    return sortedMap.lastEntry()?.value
 }

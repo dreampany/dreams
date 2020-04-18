007d44f63d6664b59575e7ee513271bdcf9e2fca
Roman <hawladar.roman@bjitgroup.com>
2020-04-18 10:48:23 +0600

    update station view model

--- tools/src/main/java/com/dreampany/tools/ui/radio/vm/StationViewModel.kt
+++ tools/src/main/java/com/dreampany/tools/ui/radio/vm/StationViewModel.kt
@@ -9,12 +9,11 @@ import com.dreampany.tools.data.enums.home.Action
 import com.dreampany.tools.data.enums.home.State
 import com.dreampany.tools.data.enums.home.Subtype
 import com.dreampany.tools.data.enums.home.Type
-import com.dreampany.tools.data.model.crypto.Coin
+import com.dreampany.tools.data.enums.radio.RadioState
 import com.dreampany.tools.data.model.radio.Station
 import com.dreampany.tools.data.source.radio.pref.RadioPref
 import com.dreampany.tools.data.source.radio.repo.StationRepo
 import com.dreampany.tools.misc.constant.RadioConstants
-import com.dreampany.tools.ui.crypto.model.CoinItem
 import com.dreampany.tools.ui.radio.model.StationItem
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -39,15 +38,9 @@ class StationViewModel
     rm
 ) {
 
-    fun loadStations(countryCode: String, offset: Long) {
+    fun loadStations(state: RadioState, countryCode: String, offset: Long) {
         uiScope.launch {
-            postProgressMultiple(
-                Type.STATION,
-                Subtype.DEFAULT,
-                State.DEFAULT,
-                Action.DEFAULT,
-                progress = true
-            )
+            postProgress(true)
             var result: List<Station>? = null
             var errors: Throwable? = null
             try {
@@ -65,32 +58,76 @@ class StationViewModel
                 errors = error
             }
             if (errors != null) {
-                postMultiple(
-                    Type.STATION,
-                    Subtype.DEFAULT,
-                    State.DEFAULT,
-                    Action.DEFAULT,
-                    error = errors,
-                    showProgress = true
-                )
+                postError(errors)
             } else if (result != null) {
-                postMultiple(
-                    Type.STATION,
-                    Subtype.DEFAULT,
-                    State.DEFAULT,
-                    Action.DEFAULT,
-                    result = result.toItems(),
-                    showProgress = true
-                )
+                postResult(result.toItems())
             }
         }
     }
 
-    suspend fun List<Station>.toItems(): List<StationItem> {
+    fun loadStations(state: RadioState) {
+        uiScope.launch {
+            postProgress(true)
+            var result: List<Station>? = null
+            var errors: Throwable? = null
+            try {
+                result = if (state == RadioState.TRENDS) repo.getItemsOfTrends(
+
+                    RadioConstants.Limits.STATIONS
+                ) else
+                    repo.getItemsOfPopular(
+                        RadioConstants.Limits.STATIONS
+                    )
+            } catch (error: SmartError) {
+                Timber.e(error)
+                errors = error
+            }
+            if (errors != null) {
+                postError(errors)
+            } else if (result != null) {
+                postResult(result.toItems())
+            }
+        }
+    }
+
+    private suspend fun List<Station>.toItems(): List<StationItem> {
         val list = this
         return withContext(Dispatchers.IO) {
             val order = pref.getStationOrder()
             list.map { StationItem(it, order) }
         }
+    }
+
+    private fun postProgress(progress: Boolean) {
+        postProgressMultiple(
+            Type.STATION,
+            Subtype.DEFAULT,
+            State.DEFAULT,
+            Action.DEFAULT,
+            progress = progress
+        )
+    }
+
+
+    private fun postError(error: Throwable) {
+        postMultiple(
+            Type.STATION,
+            Subtype.DEFAULT,
+            State.DEFAULT,
+            Action.DEFAULT,
+            error = error,
+            showProgress = true
+        )
+    }
+
+    private fun postResult(result: List<StationItem>) {
+        postMultiple(
+            Type.STATION,
+            Subtype.DEFAULT,
+            State.DEFAULT,
+            Action.DEFAULT,
+            result = result,
+            showProgress = true
+        )
     }
 }

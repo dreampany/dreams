30cc46dab779adb27761790854e03838390e57c1
Roman <hawladar.roman@bjitgroup.com>
2020-05-22 02:01:29 +0600

    Wifi Repo

--- wifi/src/main/java/com/dreampany/wifi/scan/Scanner.kt
+++ wifi/src/main/java/com/dreampany/wifi/scan/Scanner.kt
@@ -1,5 +1,8 @@
 package com.dreampany.wifi.scan
 
+import android.os.Handler
+import com.dreampany.wifi.data.source.WifiRepo
+
 /**
  * Created by roman on 21/5/20
  * Copyright (c) 2020 bjit. All rights reserved.
@@ -7,6 +10,30 @@ package com.dreampany.wifi.scan
  * Last modified $file.lastModified
  */
 class Scanner(
-
+    private val repo: WifiRepo,
+    private val handler: Handler
 ) : ScanService {
+
+    override val isScanning: Boolean
+        get() = TODO("Not yet implemented")
+
+    override fun start() {
+        TODO("Not yet implemented")
+    }
+
+    override fun startScan() {
+        TODO("Not yet implemented")
+    }
+
+    override fun stopScan() {
+        TODO("Not yet implemented")
+    }
+
+    override fun stop() {
+        TODO("Not yet implemented")
+    }
+
+    override fun takeScanResults() {
+        TODO("Not yet implemented")
+    }
 }

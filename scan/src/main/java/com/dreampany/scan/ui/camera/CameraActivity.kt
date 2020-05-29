4b8f414b35763b606a25fe83c52d89762ea23bc6
Roman <hawladar.roman@bjitgroup.com>
2020-05-29 10:35:17 +0600

    camera ui

--- scan/src/main/java/com/dreampany/scan/ui/camera/CameraActivity.kt
+++ scan/src/main/java/com/dreampany/scan/ui/camera/CameraActivity.kt
@@ -3,10 +3,13 @@ package com.dreampany.scan.ui.camera
 import android.content.Context
 import android.hardware.display.DisplayManager
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
 import android.view.View
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import com.dreampany.framework.misc.extension.FLAGS_FULLSCREEN
 import com.dreampany.framework.misc.extension.createFile
 import com.dreampany.framework.misc.extension.mediaDir
@@ -16,9 +19,15 @@ import com.dreampany.scan.R
 import com.dreampany.scan.databinding.CameraActivityBinding
 import com.dreampany.scan.databinding.CameraUiContainerBinding
 import com.dreampany.scan.misc.Constants
+import com.dreampany.scan.misc.Constants.Keys.RATIO_16_9_VALUE
+import com.dreampany.scan.misc.Constants.Keys.RATIO_4_3_VALUE
+import timber.log.Timber
 import java.io.File
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 /**
  * Created by roman on 27/5/20
@@ -39,10 +48,19 @@ class CameraActivity : InjectActivity() {
 
     private var displayId: Int = -1
     private var lensFacing: Int = CameraSelector.LENS_FACING_BACK
+    private var preview: Preview? = null
     private var imageCapture: ImageCapture? = null
-
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var cameraProvider: ProcessCameraProvider? = null
+    private var camera: Camera? = null
 
     override val layoutRes: Int = R.layout.camera_activity
+
+    private val hasBackCamera: Boolean
+        get() = cameraProvider?.hasCamera(CameraSelector.DEFAULT_BACK_CAMERA) ?: false
+
+    private val hasFrontCamera: Boolean
+        get() = cameraProvider?.hasCamera(CameraSelector.DEFAULT_FRONT_CAMERA) ?: false
 
     override fun onStartUi(state: Bundle?) {
         initUi()
@@ -67,6 +85,7 @@ class CameraActivity : InjectActivity() {
         bind.preview.post {
             displayId = bind.preview.display.displayId
             updateCameraUi()
+            initCamera()
         }
     }
 
@@ -76,6 +95,54 @@ class CameraActivity : InjectActivity() {
 
         val controls = CameraUiContainerBinding.inflate(layoutInflater, bind.layout, true)
         controls.cameraCaptureButton.setOnSafeClickListener { capture() }
+    }
+
+    private fun initCamera() {
+        val provider = ProcessCameraProvider.getInstance(this)
+        provider.addListener(Runnable {
+            cameraProvider = provider.get()
+            lensFacing = when {
+                hasBackCamera -> CameraSelector.LENS_FACING_BACK
+                hasFrontCamera -> CameraSelector.LENS_FACING_FRONT
+                else -> throw IllegalStateException("Back and front camera are unavailable")
+            }
+            bindCamera()
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun bindCamera() {
+        val metrics = DisplayMetrics().also { bind.preview.display.getRealMetrics(it) }
+        Timber.d("Screen metrics: ${metrics.widthPixels} x ${metrics.heightPixels}")
+
+        val screenAspectRatio = aspectRatio(metrics.widthPixels, metrics.heightPixels)
+        Timber.d("Preview aspect ratio: $screenAspectRatio")
+
+        val rotation = bind.preview.display.rotation
+        val cameraProvider = cameraProvider
+            ?: throw IllegalStateException("Camera initialization failed.")
+
+        val cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+
+        preview = Preview.Builder()
+            .setTargetAspectRatio(screenAspectRatio)
+            .setTargetRotation(rotation)
+            .build()
+
+        imageAnalyzer = ImageAnalysis.Builder()
+            .setTargetAspectRatio(screenAspectRatio)
+            .setTargetRotation(rotation)
+            .build()
+            .also {
+                it.setAnalyzer(cameraExecutor, BarcodeAnalyzer())
+            }
+
+        cameraProvider.unbindAll()
+        try {
+            camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalyzer)
+            preview?.setSurfaceProvider(bind.preview.createSurfaceProvider())
+        } catch (error : Throwable) {
+            Timber.e(error)
+        }
     }
 
     private fun capture() {
@@ -89,5 +156,20 @@ class CameraActivity : InjectActivity() {
                 .setMetadata(meta)
                 .build()
         }
+    }
+
+    private fun aspectRatio(width: Int, height: Int): Int {
+        val previewRatio = max(width, height).toDouble() / min(width, height)
+        if (abs(previewRatio - RATIO_4_3_VALUE) <= abs(previewRatio - RATIO_16_9_VALUE)) {
+            return AspectRatio.RATIO_4_3
+        }
+        return AspectRatio.RATIO_16_9
+    }
+
+    private class BarcodeAnalyzer : ImageAnalysis.Analyzer {
+        override fun analyze(image: ImageProxy) {
+             Timber.v("image")
+        }
+
     }
 }

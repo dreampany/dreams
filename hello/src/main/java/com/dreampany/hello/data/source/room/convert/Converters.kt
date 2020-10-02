8a59bb6dced0ddedbdd3c97dbfc8cf3164af2ca4
Roman <hawladar.roman@bjitgroup.com>
2020-10-03 00:30:02 +0600

    hello: to do register

--- hello/src/main/java/com/dreampany/hello/data/source/room/convert/Converters.kt
+++ hello/src/main/java/com/dreampany/hello/data/source/room/convert/Converters.kt
@@ -1,6 +1,8 @@
 package com.dreampany.hello.data.source.room.convert
 
+import androidx.room.TypeConverter
 import com.dreampany.framework.data.source.room.converter.Converter
+import com.dreampany.hello.data.enums.Gender
 
 /**
  * Created by roman on 26/9/20
@@ -9,4 +11,16 @@ import com.dreampany.framework.data.source.room.converter.Converter
  * Last modified $file.lastModified
  */
 class Converters : Converter() {
+
+    @Synchronized
+    @TypeConverter
+    fun toString(input: Gender?): String? {
+        return if (input == null) null else input.name
+    }
+
+    @Synchronized
+    @TypeConverter
+    fun toGender(input: String?): Gender? {
+        return if (input.isNullOrEmpty()) null else Gender.valueOf(input)
+    }
 }

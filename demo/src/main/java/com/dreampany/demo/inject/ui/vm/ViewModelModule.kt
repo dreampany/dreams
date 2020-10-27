4363d25408a74651672841a938e74489cde4cca3
Roman <hawladar.roman@bjitgroup.com>
2020-10-27 22:20:52 +0600

    demo

---
+++ demo/src/main/java/com/dreampany/demo/inject/ui/vm/ViewModelModule.kt
@@ -0,0 +1,42 @@
+package com.dreampany.demo.inject.ui.vm
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.dreampany.framework.inject.annote.ViewModelKey
+import com.dreampany.framework.ui.vm.factory.ViewModelFactory
+import com.dreampany.demo.ui.more.vm.MoreViewModel
+import com.dreampany.demo.ui.vm.SearchViewModel
+import com.dreampany.demo.ui.vm.TimeViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+import javax.inject.Singleton
+
+/**
+ * Created by roman on 21/3/20
+ * Copyright (c) 2020 bjit. All rights reserved.
+ * hawladar.roman@bjitgroup.com
+ * Last modified $file.lastModified
+ */
+@Module
+abstract class ViewModelModule {
+
+    @Singleton
+    @Binds
+    abstract fun bindFactory(factory: ViewModelFactory): ViewModelProvider.Factory
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MoreViewModel::class)
+    abstract fun bindMore(vm: MoreViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(TimeViewModel::class)
+    abstract fun bindTime(vm: TimeViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(SearchViewModel::class)
+    abstract fun bindSearch(vm: SearchViewModel): ViewModel
+}
---
+++ demo/src/main/java/com/dreampany/demo/misc/Constants.kt
@@ -0,0 +1,57 @@
+package com.dreampany.demo.misc
+
+import java.util.concurrent.TimeUnit
+
+/**
+ * Created by roman on 29/5/20
+ * Copyright (c) 2020 bjit. All rights reserved.
+ * hawladar.roman@bjitgroup.com
+ * Last modified $file.lastModified
+ */
+class Constants {
+
+    object Keys {
+        object Pref {
+            const val PREF = "pref"
+            const val MISC = "misc"
+            const val EXPIRE = "expire"
+            const val CATEGORY = "category"
+            const val PAGE = "page"
+            const val CATEGORIES = "categories"
+            const val PAGES = "pages"
+            const val SEARCH = "search"
+            const val VIDEO = "video"
+            const val VIDEOS = "videos"
+        }
+
+        object Room {
+            const val ROOM = "room"
+            const val MISC = "misc"
+        }
+
+        object Related {
+            const val LEFTER = "lefter"
+            const val RIGHTER = "righter"
+        }
+    }
+
+    object Values {
+        const val VIDEOS = "tube.videos"
+        const val SEARCH = "tube.search"
+    }
+
+    object Limits {
+        const val VIDEOS = 50L
+    }
+
+    object Times {
+        val HOUSE_ADS = TimeUnit.HOURS.toMillis(1)
+        val CATEGORIES = TimeUnit.DAYS.toMillis(7)
+        val VIDEOS = TimeUnit.DAYS.toMillis(1)
+        val VIDEO = TimeUnit.HOURS.toMillis(1)
+    }
+
+    object Count {
+        const val MIN_CATEGORIES = 3
+    }
+}

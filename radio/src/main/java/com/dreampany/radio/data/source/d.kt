58cfc9a878607fe61df84a61a8b45690b3e556ad
Roman <hawladar.roman@bjitgroup.com>
2020-10-28 00:02:55 +0600

    radio added

---
+++ radio/src/main/java/com/dreampany/radio/data/source/d.kt
@@ -0,0 +1,9 @@
+package com.dreampany.radio.data.source
+
+/**
+ * Created by roman on 27/10/20
+ * Copyright (c) 2020 bjit. All rights reserved.
+ * hawladar.roman@bjitgroup.com
+ * Last modified $file.lastModified
+ */
+ 
---
+++ radio/src/main/java/com/dreampany/radio/inject/data/DataModule.kt
@@ -0,0 +1,19 @@
+package com.dreampany.radio.inject.data
+
+import com.dreampany.framework.inject.data.DatabaseModule
+import dagger.Module
+
+/**
+ * Created by roman on 5/6/20
+ * Copyright (c) 2020 bjit. All rights reserved.
+ * hawladar.roman@bjitgroup.com
+ * Last modified $file.lastModified
+ */
+@Module(
+    includes = [
+        DatabaseModule::class,
+        MiscModule::class
+    ]
+)
+class DataModule {
+}

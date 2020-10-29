b74388d91a3d62a433cfa328a4c70759112338c6
Roman <hawladar.roman@bjitgroup.com>
2020-10-29 14:27:03 +0600

    news: settings to pages

--- news/src/main/java/com/dreampany/news/ui/settings/SettingsFragment.kt
+++ news/src/main/java/com/dreampany/news/ui/settings/SettingsFragment.kt
@@ -5,8 +5,10 @@ import androidx.preference.Preference
 import com.dreampany.news.R
 import com.dreampany.framework.inject.annote.ActivityScope
 import com.dreampany.framework.misc.exts.moreApps
+import com.dreampany.framework.misc.exts.open
 import com.dreampany.framework.misc.exts.rateUs
 import com.dreampany.framework.ui.fragment.InjectFragment
+import com.dreampany.news.ui.page.PagesActivity
 import com.mikepenz.aboutlibraries.LibsBuilder
 import javax.inject.Inject
 
@@ -30,7 +32,10 @@ class SettingsFragment
     }
 
     override fun onPreferenceTreeClick(preference: Preference): Boolean {
-        if (preference.key == getString(R.string.key_settings_more_apps)) {
+        if (preference.key == getString(R.string.key_settings_pages)) {
+            openPagesUi()
+            return true
+        }  else if (preference.key == getString(R.string.key_settings_more_apps)) {
             moreApps()
             return true
         } else if (preference.key == getString(R.string.key_settings_rate_us)) {
@@ -48,6 +53,17 @@ class SettingsFragment
 
     private fun initUi() {
 
+    }
+
+    private fun openPagesUi() {
+        /*val task = UiTask(
+            Type.CATEGORY,
+            Subtype.DEFAULT,
+            State.DEFAULT,
+            Action.BACK,
+            null as Category?
+        )*/
+        open(PagesActivity::class)
     }
 
     private fun moreApps() {
